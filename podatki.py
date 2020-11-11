10fa8cfcb2fb7dca50e9c88e4f047e622d2b6d73
DejanPeric058 <najedcirep@gmail.com>
2020-11-11 10:15:53 +0100

    pridobil nekaj podatkov, zaradi omejitve 30 na dan jih bom pridobival do decembra

--- podatki.py
+++ podatki.py
@@ -11,8 +11,7 @@ vzorec_podrobnosti = re.compile(
     r'Delo:.*?<td class="tdDataMain">\s{1,100}(?P<delo>.*?)\s{1,100}</td>.*?'
     r'Kraj / Regija:.*?<td class="tdDataMain">\s{1,100}(?P<kraj>.*?)\s{1,100}'
     r'<br/>\s{1,100}\((?P<Regija>.*?)\)\s{1,100}</td>.*?'
-    r'Plačilo:.*?<td class="tdDataMain">\s{1,100}(?P<neto>.*?)\sNETO,\s'
-    r'(?P<bruto>.*?)\sBRUTO.*?'
+    r'Plačilo:.*?<td class="tdDataMain">\s{1,100}(?P<plačilo>.*?)\s{1,100}</td>.*?'
     r'Naslov:.*?<td class="tdData" style="">\s{1,100}(?P<naslov>.*?)\s{1,100}</td>.*?'
     r'Datum pričetka:.*?<td class="tdData">\s{1,100}(?P<datum_pričetka>.*?)\s{1,100}</td>.*?'
     r'Delovnik:.*?<td class="tdData">\s{1,100}(?P<delovnik>.*?)\s{1,100}</td>.*?'
@@ -40,18 +39,19 @@ def izloci_podatke_oglasa(vsebina):
 
 orodja.shrani_spletno_stran(URL, 'zajeti-podatki/prosta-dela.html')
 vsebina = orodja.vsebina_datoteke('zajeti-podatki/prosta-dela.html')
-sifre = re.findall(vzorec_sifre, vsebina, flags=re.DOTALL)
+#sifre = re.findall(vzorec_sifre, vsebina, flags=re.DOTALL)
+sifre = ['326859', '326855', '326854', '326853', '326852', '326851', '326850', '326849', '326845', '326842', '326841', '326840', '326839', '326838', '326837', '326836', '326834', '326832', '326830', '326829', '326828', '326827', '326826', '326824', '326823', '326822', '326820', '326819', '326818', '326817', '326814', '326812', '326810', '326809', '326808', '326807', '326804', '326803', '326802', '326799', '326798', '326796', '326795', '326792', '326791', '326790', '326789', '326788', '326787', '326785', '326780', '326778', '326777', '326776', '326775', '326774']
 
 oglasi = []
 
-#for sifra in sifre:
-#    oglasi.append(podrobnosti(sifra))
-#orodja.zapisi_json(oglasi, 'obdelani-podatki/oglasi.json')
-#orodja.zapisi_csv(
-#    oglasi,
-#    ['delo', 'kraj', 'Regija', 'neto', 'bruto', 'naslov', 'datum_pričetka', 'delovnik', 'število_članov', 'trajanje', 'šifra', 'opis', 'podjetje'],
-#    'obdelani-podatki/oglasi.csv'
-#    )
+for sifra in sifre:
+    oglasi.append(podrobnosti(sifra))
+orodja.zapisi_json(oglasi, 'obdelani-podatki/oglasi.json')
+orodja.zapisi_csv(
+    oglasi,
+    ['delo', 'kraj', 'Regija', 'plačilo', 'naslov', 'datum_pričetka', 'delovnik', 'število_članov', 'trajanje', 'šifra', 'opis', 'podjetje'],
+    'obdelani-podatki/oglasi.csv'
+    )
 
 print(len(sifre))
 print(len(oglasi))
